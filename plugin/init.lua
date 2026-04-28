5f99fe30e567f74b01ced6b10cad0e9f5a8b4ebe
Matt Johnson <infernox64@gmail.com>
2026-04-28 16:49:56 -0400

    Use config for luarocks_path in init.lua

--- plugin/init.lua
+++ plugin/init.lua
@@ -67,7 +67,7 @@ local default_config = {
     show_loading = true,
     type = "local",
     api_key = nil, -- Only used for Google API
-    luarocks_path = "/usr/local/bin/luarocks", -- Default path to luarocks binary
+    luarocks_path = config.luarocks_path, -- Default path to luarocks binary
 }
 
 local function get_provider(config)
